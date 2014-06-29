7b0a377b171b5eedf878b54b2f3060862de3b76a
cash <cash@cashwu.com>
2014-06-30 00:13:42 +0800

    v1.2 Layout

---
+++ scripts/blog.js
@@ -0,0 +1,10 @@
+
+// var blogApp = angular.module("blogApp",["mgcrea.ngStrap"]);
+
+// blogApp.controller("ContentCtrl", function($scope){
+
+// });
+
+// angular.element(document).ready(function(){
+//     angular.bootstrap(document, ["blogApp"]);
+// });
