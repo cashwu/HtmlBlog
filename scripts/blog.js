3ed0417370b6f27269778a73bd64b1c426204946
cash <cash@cashwu.com>
2014-07-01 00:33:26 +0800

    v1.3 Add ng & 2014/06/30 Start

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,10 +1,39 @@
 
-// var blogApp = angular.module("blogApp",["mgcrea.ngStrap"]);
+//angular.element(document).ready(function(){
+//    angular.bootstrap(document, ["blogApp"]);
+//});
 
-// blogApp.controller("ContentCtrl", function($scope){
+var blogApp = angular.module("blogApp",["ngRoute"]);
 
-// });
+blogApp.controller("blogController", function($scope){
+    initMarkdown();
+});
 
-// angular.element(document).ready(function(){
-//     angular.bootstrap(document, ["blogApp"]);
-// });
+function blogAppConfig($routeProvider){
+    $routeProvider
+        .when("/",{
+            templateUrl: "Views/default.html",
+            controller: "blogController"
+        })
+        .when("/2014/06/30/Start",{
+            templateUrl: "Views/2014/06/30/Start.html",
+            controller: "blogController"
+        })
+        .otherwise({
+            redirectTo: "/"
+        });
+}
+
+blogApp.config(blogAppConfig);
+
+function initMarkdown(){
+    var markdown = document.getElementById("markdown");
+    if (markdown){
+        var converter = new Showdown.converter();
+        var markdowncontent = markdown.innerHTML;
+        var markdownhtml = converter.makeHtml(markdowncontent);
+        document.getElementById("content").innerHTML = markdownhtml;
+    } else {
+        document.getElementById("content").innerHTML = "";
+    }
+}
