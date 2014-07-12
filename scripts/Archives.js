ec96cf20f14674295d69d0d56caefdfdab71ea68
cash <cash@cashwu.com>
2014-07-12 22:44:20 +0800

    v2.6 Add Archives subtitle

--- scripts/Archives.js
+++ scripts/Archives.js
@@ -10,37 +10,43 @@ var archives = [
         year : "",
         date : "07/12",
         url : "http://blog.cashwu.com/#/2014/07/12/DB-Develop04",
-        title: "ASP.NET MVC 使用資料庫開發 (四)"
+        title: "ASP.NET MVC 使用資料庫開發 (四)",
+        subtitle: "當資料庫有變動時 從資料庫更新模型時 不一定會更新"
     },
     {
         year : "",
         date : "07/12",
         url : "http://blog.cashwu.com/#/2014/07/12/DB-Develop03",
-        title: "ASP.NET MVC 使用資料庫開發 (三)"
+        title: "ASP.NET MVC 使用資料庫開發 (三)",
+        subtitle: "資料庫欄位有預設值時處理"
     },
     {
         year : "",
         date : "07/10",
         url : "http://blog.cashwu.com/#/2014/07/10/DB-Develop02",
-        title: "ASP.NET MVC 使用資料庫開發 (二)"
+        title: "ASP.NET MVC 使用資料庫開發 (二)",
+        subtitle: "使用 Entity Framework 作 CRUD"
     },
     {
         year : "",
         date : "07/09",
         url : "http://blog.cashwu.com/#/2014/07/09/DB-Develop01",
-        title: "ASP.NET MVC 使用資料庫開發 (一)"
+        title: "ASP.NET MVC 使用資料庫開發 (一)",
+        subtitle: "建立資料庫模型、POCO、ORM、Entity Framework、Entity Framework 連線字串"
     },
     {
         year : "",
         date : "07/07",
         url : "http://blog.cashwu.com/#/2014/07/07/Second-MVC-Class",
-        title: "第二次的 保哥 MVC 課程"
+        title: "第二次的 保哥 MVC 課程",
+        subtitle: ""
     },
     {
         year : "",
         date : "06/30",
         url : "http://blog.cashwu.com/#/2014/06/30/Start",
-        title: "Start"
+        title: "Start",
+        subtitle: ""
     },
     {
         year : "2012"
@@ -49,12 +55,14 @@ var archives = [
         year : "",
         date : "06/05",
         url : "http://blog.cashwu.com/#/2012/06/13/Ubuntu-Disable-Touchpad",
-        title: "在 Ubuntu 停用觸控板"
+        title: "在 Ubuntu 停用觸控板",
+        subtitle: ""
     },
     {
         year : "",
         date : "06/05",
         url : "http://blog.cashwu.com/#/2012/06/05/Ubuntu-ISO",
-        title: "在 Ubuntu 下建立光碟 ISO 檔"
+        title: "在 Ubuntu 下建立光碟 ISO 檔",
+        subtitle: ""
     }
 ];
